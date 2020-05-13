14eab85062b60939fe2d41e8506083b128a1fbd3
Ronda D Cowen <cowenhome5@gmail.com>
2020-05-13 23:03:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,8 @@ class ApplicationController < Sinatra::Base
     if @user
       session[:user_id] = @user.id
       redirect to '/account'
-      
+      "Welcome, <%=@user.name%>!"
+
     end
     redirect erb :error
 
