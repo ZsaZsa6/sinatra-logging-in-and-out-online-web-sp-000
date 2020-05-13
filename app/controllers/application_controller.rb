b4c71d9514aa872f69cab9408a684c0d9777922c
Ronda D Cowen <cowenhome5@gmail.com>
2020-05-13 22:02:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(username: params[:username])
     if @user
-      session[:user_id] = @user.id
+      [:user_id] = @user.id
       redirect '/users/home'
     end
 
